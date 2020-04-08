3ad8040aba63958d87d36a2d9bae6b4f6a6d17c5
木瓜 <liyuning.tan@qq.com>
2020-04-08 15:58:18 +0800

    '中介者模式'

---
+++ src/entries/intermediary/Player.ts
@@ -0,0 +1,40 @@
+import { PlayerDirectorType } from './index'
+export type Team = 'blue' | 'red'
+export type Alive = 'alive' | 'died'
+
+export interface PlayerType {
+  name: string
+  team: Team
+  alive: Alive
+  playerDirector: PlayerDirectorType
+  win(): void
+  loose(): void
+  die(): void
+}
+
+export class Player implements PlayerType {
+  public name: string
+  public team: Team
+  public alive: Alive
+  public playerDirector: PlayerDirectorType
+  constructor(name: string, team: Team, playerDirector: PlayerDirectorType) {
+    this.name = name
+    this.team = team
+    this.alive = 'alive'
+    this.playerDirector = playerDirector
+  }
+
+  public win() {
+    document.write(`${this.name}获胜<br/>`)
+  }
+
+  public loose() {
+    document.write(`${this.name}失败<br/>`)
+  }
+
+  public die() {
+    document.write(`${this.name}阵亡<br/>`)
+    this.alive = 'died'
+    this.playerDirector.checkGameOver()
+  }
+}
---
+++ src/entries/intermediary/index.ts
@@ -0,0 +1,69 @@
+import { Player, Team } from './Player'
+// 中介者模式思想实现
+
+/*
+  创造一个中介者来解除对象与对象之间的紧耦关系
+  所有对象通过一个中介者来通信
+  当一个对象发生改变时只需通知中介者即可
+
+  遵循了【最少知识原则】，即一个对象应尽可能少地了解另外的对象
+*/
+
+/*
+  这里举了一个分组比赛的例子来展现中介者模式思想
+*/
+
+export interface PlayerDirectorType {
+  addPlayer(player: Player): PlayerDirectorType
+  checkGameOver(): void
+}
+
+interface PlayersType {
+  [team: string]: Player[]
+}
+
+class PlayerDirector implements PlayerDirectorType {
+  private players: PlayersType = {
+    red: [],
+    blue: [],
+  }
+
+  public addPlayer(player: Player): PlayerDirector {
+    this.players[player.team].push(player)
+    return this
+  }
+
+  public checkGameOver(): void {
+    let winnerTeam = ''
+    let looseTeam = ''
+    if (this.players.red.every((player) => player.alive === 'died')) {
+      winnerTeam = 'blue'
+      looseTeam = 'red'
+    }
+    if (this.players.blue.every((player) => player.alive === 'died')) {
+      winnerTeam = 'red'
+      looseTeam = 'blue'
+    }
+    if (winnerTeam) {
+      this.players[winnerTeam].forEach((player) => player.win())
+      this.players[looseTeam].forEach((player) => player.loose())
+    }
+  }
+}
+
+const playerDirector = new PlayerDirector()
+
+const Jack = new Player('Jack', 'red', playerDirector)
+const Bob = new Player('Bob', 'red', playerDirector)
+const Faker = new Player('Faker', 'blue', playerDirector)
+const Rose = new Player('Zoom', 'blue', playerDirector)
+
+playerDirector
+  .addPlayer(Jack)
+  .addPlayer(Bob)
+  .addPlayer(Faker)
+  .addPlayer(Rose)
+
+Jack.die()
+Rose.die()
+Bob.die()
